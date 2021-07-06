98cc26e31a6e2dc877dec6b672a1561bf13cf56e
Diego <diego17almoradi@gmail.com>
2021-07-06 21:16:41 +0200

    Commit inicial

---
+++ PruebaTecnicaRest/src/test/java/es/dev/center/prueba/PruebaTecnicaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package es.dev.center.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
